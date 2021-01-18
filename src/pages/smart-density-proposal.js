6a8ddf957cddcf81e04e584c152296f92b2c71e5
Hana Drdla <>
2021-01-18 13:12:34 -0600

    SD proposal

---
+++ src/pages/smart-density-proposal.js
@@ -0,0 +1,192 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SmartDensityProposal = () => {
+  return (
+    <Layout>
+      <SEO title="Smart Density Proposal" />
+      <article>
+        <div className="center">
+          <h2>Website Proposal</h2>
+          <h1>SmartDensity.com</h1>
+        </div>
+        <div className="page-divider"></div>
+        <h3>Overview</h3>
+        <p>
+          Smart Density is an architecture firm located in Toronto, Canada.
+          Smart Density offers urban planning and architecture services, free
+          and paid webinars, and downloadable resources. They require a website
+          redesign that better reflects the quality of their services, and
+          attracts a clientelle with a larger budget.
+        </p>
+        <h3>What's Included</h3>
+        <ul>
+          <li>
+            Website rebuild using Elementor page builder for easy updating
+          </li>
+          <li>Professional and elegant design with architectural style</li>
+          <li>
+            Responsive design for optimal viewing on desktop, tablet and mobile
+          </li>
+          <li>
+            Simple, user-friendly navigation that encourages users to browse
+            further
+          </li>
+          <li>Fast to load and compatible across all modern browsers</li>
+          <li>30 days of website support following website launch</li>
+          <li>
+            <li>
+              Recorded training video so you know how to edit and maintain your
+              website
+            </li>
+            <li>Rebuild of 18-19 pages</li>
+            <ol className="sub-list">
+              <li>Home</li>
+              <li>Work (/mixed-use-development)</li>
+              <li>Missing Middle (/missing-middle)</li>
+              <li>Missing Middle - Start Here (/missing-middle-start-here)</li>
+              <li>Missing Middle - Projects</li>
+              <li>Missing Middle - Videos</li>
+              <li>Mixed-Use (/mixed-use-development)</li>
+              <li>
+                Highest and Best Use Studies
+                (/feasibility-studies-planning-urban-design)
+              </li>
+              <li>Feasibility DIY (/developers-toolkit)</li>
+              <li>Community Engagement</li>
+              <li>Learn (/webinars)</li>
+              <li>Blog about urban issues</li>
+              <li>From the Ground Up (/how-we-built-this-toronto)</li>
+              <li>Our Team</li>
+              <li>Why Us?</li>
+              <li>Smart Density in the Media </li>
+              <li>Media Inquiries </li>
+              <li>Contact</li>
+              <li>Addition of Small-Scale project page?</li>
+            </ol>
+          </li>
+          <li>1 Portfolio page template</li>
+          <li>1 Blog post template</li>
+        </ul>
+        <br />
+        <div className="price">
+          <p>
+            An investment of <br />
+            <span className="dollar">$4000</span>{" "}
+            <span className="accent">CAD + HST</span>
+          </p>
+        </div>
+        <br />
+
+        <h4>Larger Budget?</h4>
+        <p>
+          Consider the complete package. This package additionally includes
+          reorganizing exisiting blog posts and portfolio pieces to fit the
+          templates:
+        </p>
+        <ul>
+          <li>5 Missing Middle portfolio pages</li>
+          <li>28 Mixed-Use portfolio pages</li>
+          <li>32 blog articles</li>
+          <p>(65 pages total)</p>
+        </ul>
+        <div className="price">
+          <p>
+            An investment of <br />
+            <span className="dollar">$5000</span>{" "}
+            <span className="accent">CAD + HST</span>
+          </p>
+        </div>
+        <br />
+
+        <h3>Process (4-5 weeks)</h3>
+        <br />
+        <p>
+          <strong>
+            **Example timeline: January 20, 2021 - February 24, 2021**
+          </strong>
+        </p>
+        <h4>Before Start Date</h4>
+        <p>
+          <strong>1. Content Preparation (client side)</strong>
+        </p>
+        <ul>
+          <li>Sign contract and provide 50% deposit</li>
+          <li>Provide Wordpress, website hosting, and domain login details</li>
+          <li>
+            Provide examples of websites you like the visual style of (and any
+            notes if applicable)
+          </li>
+          <li>
+            Provide any website copy that will be added (for example, Small
+            scale project page, LinkedIn profile links for team members)
+          </li>
+        </ul>
+        <br />
+        <h4>Project Start</h4>
+        <p>
+          <strong>2. Website Development (Weeks 1-3)</strong>
+        </p>
+        <ul>
+          <li>
+            Design & build all website pages on a different domain (so not to
+            disturb your current web presence)
+          </li>
+          <li>Weekly updates during this time</li>
+        </ul>
+        <br />
+        <p>
+          <strong>3. Website Review (Weeks 4-5)</strong>
+        </p>
+        <ul>
+          <li>Have 2 opportunities for revision</li>
+          <li>
+            Please provide all feedback within 3 work days and all in one email
+          </li>
+        </ul>
+        <br />
+        <p>
+          <strong>4. Website Handoff (End of Week 5)</strong>
+        </p>
+        <ul>
+          <li>Final 50% Payment</li>
+          <li>Connect website domain and final browser & mobile testing</li>
+        </ul>
+        <br />
+        <p>
+          <strong>5. Continued Support (30 days from handoff)</strong>
+        </p>
+        <ul>
+          <li>Continued email support to fix any bugs or help with editing</li>
+        </ul>
+        <br />
+
+        <br />
+        <h3>Payment & Delivery Terms</h3>
+        <p>
+          50% payment up front is required to commence work, with the balance
+          payable upon completion. This can be paid via bank transfer.
+        </p>
+        <p>
+          Once the final payment is made it will take 24 to 48 hours to connect
+          the domain name and complete final browser testing of the site.
+        </p>
+        <h2>Accept the Proposal</h2>
+        <p className="large">
+          To accept the proposal and commence work please:
+        </p>
+        <p>
+          Send an email confirmation to <strong>hanadrdla@gmail.com</strong>
+          .<br />
+          You will receive a digital contract from HelloSign within 24 hours.
+          <br />
+          For direct debit payments, please transfer to{" "}
+          <strong>hanadrdla@gmail.com</strong>.
+        </p>
+      </article>
+    </Layout>
+  )
+}
+
+export default SmartDensityProposal
